bc6f37afc2625f29557bb334d9bcd11c78de3d7c
kris-p19 <kris.p.pop@gmail.com>
2024-05-31 14:14:22 +0700

    dev

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,32 @@
 $(document).ready(function(){
-    fetch('/resouce/layout/navbar.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('navbar-container').innerHTML = data;
-        })
-        .catch(error => console.error('Error loading navbar:', error));
+    fetch('/resource/layouts/navbar.html')
+    .then(response => response.text())
+    .then(data => {
+        $('#navbar-container').html(data);
+    })
+    .catch(error => console.error('Error loading navbar:', error));
+
+    fetch('/resource/layouts/footer.html')
+    .then(response => response.text())
+    .then(data => {
+        $('#footer-container').html(data);
+    })
+    .catch(error => console.error('Error loading footer:', error));
+
+    const params = new URLSearchParams(window.location.search);
+    const page = params.get('page') || 'home';
+    fetch(`/resource/views/${page}.html`)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.text();
+    })
+    .then(data => {
+        $('#content-container').html(data);
+    })
+    .catch(error => {
+        $('#content-container').html('<p>Sorry, the requested page could not be loaded.</p>');
+        console.error('Error loading page:', error);
+    });
 });
