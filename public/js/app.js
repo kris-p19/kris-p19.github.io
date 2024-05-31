d9f6856d640d277cb00dba6196c63ec381c72bbe
kris-p19 <kris.p.pop@gmail.com>
2024-05-31 13:55:43 +0700

    dev

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+$(document).ready(function(){
     fetch('/resouce/layout/navbar.html')
         .then(response => response.text())
         .then(data => {
