60854a3e90eeb062c0caf68500160aad0db61b4d
kris-p19 <kris.p.pop@gmail.com>
2024-05-31 13:45:53 +0700

    dev

---
+++ public/js/app.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetch('/resouce/layout/navbar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('navbar-container').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading navbar:', error));
+});
