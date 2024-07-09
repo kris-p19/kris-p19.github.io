700907c795e33f1c76fbdfa6a8aeeb9a265532bc
kris-p19 <kris.p.pop@gmail.com>
2024-07-09 22:13:44 +0700

    dev

---
+++ public/js/init-backlink.js
@@ -0,0 +1,24 @@
+let backlink = [
+    {
+        href:"/post/2024/07/03/article-001/html?nocache=" + Math.random(),
+        text:"คอมพิวเตอร์ตั้งแต่กำเนิดจนถึงปัจจุบัน",
+        target:"_blank",
+        rel:"noopener noreferrer"
+    }
+];
+
+function createBacklink(obj) {
+    var backlink = document.createElement('a');
+    backlink.href = obj.href;  // URL ของ Backlink
+    backlink.textContent = obj.text;
+    backlink.target = obj.target;  // เปิดลิงก์ในแท็บใหม่
+    backlink.rel = obj.rel;  // เพิ่มความปลอดภัย
+
+    // เลือกตำแหน่งที่ต้องการเพิ่ม Backlink
+    var content = document.getElementById('content-backlink');
+    content.appendChild(backlink);
+}
+
+backlink.forEach(element => {
+    createBacklink(element);
+});
