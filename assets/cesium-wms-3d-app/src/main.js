67308568e352cd79617b48aea6231c70ed0e2551
Kris-p19 <kris.p.pop@gmail.com>
2025-06-11 19:25:19 +0700

    dde

--- assets/cesium-wms-3d-app/src/main.js
+++ assets/cesium-wms-3d-app/src/main.js
@@ -1,24 +1,17 @@
 const viewer = new Cesium.Viewer('cesiumContainer', {
     terrainProvider: Cesium.createWorldTerrain(),
     baseLayerPicker: false
-});
+  });
 
+// เพิ่ม WMS Layer
 const wmsLayer = viewer.imageryLayers.addImageryProvider(
     new Cesium.WebMapServiceImageryProvider({
-        url: 'https://gibs.earthdata.nasa.gov/wms/epsg4326/best/wms.cgi',
-        layers: 'MODIS_Terra_CorrectedReflectance_TrueColor',
-        parameters: {
-            service: 'WMS',
-            version: '1.1.1',
-            request: 'GetMap',
-            styles: '',
-            format: 'image/png',
-            transparent: true
-        },
-        credit: 'NASA Global Imagery Browse Services'
+      url: 'https://gis.dmcr.go.th/geoserver/dmcr-wms-public/wms',
+      layers: 'area_coral',
+      parameters: {
+        service: 'WMS',
+        format: 'image/png',
+        transparent: true
+      }
     })
 );
-
-viewer.camera.flyTo({
-    destination: Cesium.Cartesian3.fromDegrees(100.5018, 13.7563, 1500000)
-});
